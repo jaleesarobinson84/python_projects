b7dca8ead0a22e7403e815611c394b749b78c017
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-04-29 19:18:21 -0400

    complete function practice

--- functions_practice.py
+++ functions_practice.py
@@ -1,12 +1,23 @@
 def hello():
-    print("Welcome to learning Python!")
+    print("Hello!!! Welcome to the world of Python!!!")
 
 def pack(x, y, z):
-    return (x, y, z)
+    return [x, y, z]
 
-
-def eat_lunch(food_list):
-    if not food_list:
-        print("First I eat", food_list[0])
-        for food in food_list[1:]:
-            print("Next I eat", food)
+# python function, function takes list as an argument
+def eat_lunch(my_list):
+  if len(my_list) == 0:
+    print("My lunchbox is empty!")
+  else:
+    # loop checks if current element is the first on the list
+    for i in range(len(my_list)):
+      if i == 0:
+        print(f"First I eat a {my_list[0]}")
+      else:
+        print(f"Next I eat {my_list[i]}")
+# output prints
+hello()
+print(pack("x", "y", "z"))
+eat_lunch([])
+eat_lunch(["burger"])
+eat_lunch(["burger", "fries", "slice of cake"])
